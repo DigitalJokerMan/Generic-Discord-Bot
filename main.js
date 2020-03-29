8c308f207793f557ee6b6089e48b64e217f129d5
AnEverydayZomby <mcshamroq@gmail.com>
2020-03-29 14:34:01 -0400

    Update main.js

--- main.js
+++ main.js
@@ -40,6 +40,7 @@ client.on('message', msg => {
           case 'topoftheday':
             if (splitted.length == 2) {
               subreddit_json = getTop(splitted[1]);
+              console.log(subreddit_json.author);
               user_json = getUser(subreddit_json.author);
               embed = {
                 "title": subreddit_json.title,
