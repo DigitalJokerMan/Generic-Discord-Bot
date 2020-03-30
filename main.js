43f73b96a98d4c0df5fe0b339ab5ec037e50a054
AnEverydayZombie <mcshamroq@gmail.com>
2020-03-30 17:31:51 -0400

    Update main.js

--- main.js
+++ main.js
@@ -22,10 +22,10 @@ async function redditGet(subreddit, iscustom, arguments) {
         } else if (iscustom == true && arguments != 'undefined') {
             var finalarguments = arguments;
             if (arguments.includes('?limit=')) {
-                var limit = /\?limit=\(\d+)/.exec(finalarguments);
+                var limit = /\?\l\i\m\i\t\=\(\d+)/.exec(finalarguments);
                 finalarguments.replace(`?limit=${limit[1]}`, '?limit=1');
             } else if (arguments.includes('?count=')) {
-                var count = /\?count=\(\d+)/.exec(finalarguments);
+                var count = /\?\c\o\u\n\t\=(\d+)/.exec(finalarguments);
                 finalarguments.replace(`?count=${count[1]}`, '?count=1');
             } else if (!arguments.includes('random.json') || !arguments.includes('random/.json')) {
                 finalarguments = finalarguments + "?limit=1"
