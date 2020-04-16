472f1a702db10e8ef923d17ae37ad1d3783f510e
AnEverydayZombie <mcshamroq@gmail.com>
2020-04-16 14:28:58 -0400

    Update main.js

--- main.js
+++ main.js
@@ -68,6 +68,7 @@ const commands = {
             const embed = new Discord.MessageEmbed()
                 .setTitle('List of all commands.')
                 .setTimestamp(new Date().now)
+                .setColor('#ff0000')
             for (const command in commands) {
                 const actual = commands[command];
                 if (command != 'help') {
