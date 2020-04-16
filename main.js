1d8341d49f2ed2e9364ceee737b9e223149919c8
AnEverydayZombie <mcshamroq@gmail.com>
2020-04-16 14:50:26 -0400

    Update main.js

--- main.js
+++ main.js
@@ -73,9 +73,7 @@ const commands = {
                 const actual = commands[command];
                 if (command != 'help') {
                     if (actual.permissions.length > 0) {
-                        console.log(command + permissions);
                         if (actual.permissions.every((perm) => message.member.hasPermission(perm))) {
-                            console.log(true);
                             const field = new Object();
                             field.name = prefix + command;
                             field.value = actual.description
