18d49f7e3c36dc615ccd4824aeebc92eac88507a
AnEverydayZombie <mcshamroq@gmail.com>
2020-04-17 20:23:31 -0400

    Update main.js

--- main.js
+++ main.js
@@ -114,7 +114,6 @@ const commands = {
             for (i=0; i<array.length; i++) {
                 console.log(array[i]);
             }
-            console.log(msg);
         },
         'permissions': []
     }
