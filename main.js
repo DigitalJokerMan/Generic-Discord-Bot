c5532c79e6f98297eac8a428e8b56608252853b2
AnEverydayZombie <mcshamroq@gmail.com>
2020-06-13 16:43:55 -0400

    Update main.js

--- main.js
+++ main.js
@@ -187,7 +187,7 @@ client.on('message', message => {
         }
     }
     if (range(0, 100) >= 0) {
-        imgur.search('cursed images', {sort: 'time', dateRange: 'day', page: 1})
+        imgur.search('cursed images', {sort: 'time', dateRange: 'all', page: 1})
             .then(json => {
                 const data = json.data; console.log(typeof data)
                 const keys = Object.keys(data); console.log(keys)
